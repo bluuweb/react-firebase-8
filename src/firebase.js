bb9a62fb70a0e8007a6b7bc9dd7da4af83c0938b
bluuweb <contacto@bluuweb.cl>
2022-06-28 12:36:48 -0400

    firebaes 8

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import app from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "xxx",
+    authDomain: "xxx",
+    projectId: "xxx",
+    storageBucket: "xxx",
+    messagingSenderId: "xxx",
+    appId: "xxx",
+};
+
+app.initializeApp(firebaseConfig);
+
+const db = app.firestore();
+const auth = app.auth();
+
+export { auth, db };
